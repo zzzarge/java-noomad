10e67f23a1707ed8f933c6b6059c60ea56e5e7b5
zzzarge <nurmuhammedumarov8111@gmail.com>
2024-09-27 22:30:33 +0600

    Beecrowd

--- src/Main.java
+++ src/Main.java
@@ -86,3 +86,97 @@
 //        }
 //    }
 //}
+//1253
+//public class Main {
+//
+//    public static String caesarDecipher(String text, int shift) {
+//        StringBuilder result = new StringBuilder();
+//
+//        for (int i = 0; i < text.length(); i++) {
+//            char ch = text.charAt(i);
+//            char shiftedChar = (char) (ch - shift);
+//            if (shiftedChar < 'A') {
+//                shiftedChar += 26;
+//            }
+//            result.append(shiftedChar);
+//        }
+//
+//        return result.toString();
+//    }
+//
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int N = scanner.nextInt();
+//        scanner.nextLine();
+//
+//        for (int i = 0; i < N; i++) {
+//            String codifiedText = scanner.nextLine();
+//            int shift = scanner.nextInt();
+//            scanner.nextLine();
+//
+//            System.out.println(caesarDecipher(codifiedText, shift));
+//        }
+//    }
+//}
+//1237
+//public class Main {
+//
+//    public static int findLongestCommonSubstring(String str1, String str2) {
+//        int maxLength = 0;
+//
+//        int len1 = str1.length();
+//        int len2 = str2.length();
+//
+//        int[][] dp = new int[len1 + 1][len2 + 1];
+//
+//        for (int i = 1; i <= len1; i++) {
+//            for (int j = 1; j <= len2; j++) {
+//                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+//                    dp[i][j] = dp[i - 1][j - 1] + 1;
+//                    maxLength = Math.max(maxLength, dp[i][j]);
+//                } else {
+//                    dp[i][j] = 0;
+//                }
+//            }
+//        }
+//
+//        return maxLength;
+//    }
+//
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        while (scanner.hasNextLine()) {
+//            String str1 = scanner.nextLine();
+//            String str2 = scanner.nextLine();
+//
+//            System.out.println(findLongestCommonSubstring(str1, str2));
+//        }
+//    }
+//}
+
+//1120
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        while (true) {
+//            String D = scanner.next();
+//            String N = scanner.next();
+//
+//            if (D.equals("0") && N.equals("0")) {
+//                break;
+//            }
+//
+//            String result = N.replace(D, "");
+//
+//            if (result.isEmpty()) {
+//                System.out.println(0);
+//            } else {
+//                System.out.println(result.replaceFirst("^0+(?!$)", ""));
+//            }
+//        }
+//    }
+//}
